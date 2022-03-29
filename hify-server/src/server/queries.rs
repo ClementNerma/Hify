d9b84e92092f6318844ba117c70d3aa844f690f6
Clément Nerma <clement.nerma@gmail.com>
2022-03-29 18:16:46 +0200

    Only allow to get index's fingerprint from server

--- hify-server/src/server/queries.rs
+++ hify-server/src/server/queries.rs
@@ -1,18 +1,16 @@
 use juniper::graphql_object;
 
-use crate::index::Index;
-
 use super::graphql::GraphQLContext;
 
 pub struct QueryRoot;
 
 #[graphql_object(context = GraphQLContext)]
 impl QueryRoot {
-    async fn index(ctx: &GraphQLContext, fingerprint: String) -> Option<Index> {
+    async fn index_fingerprint(ctx: &GraphQLContext) -> Option<String> {
         ctx.index
             .read()
             .await
-            .clone()
-            .filter(|index| index.creation_time != fingerprint)
+            .as_ref()
+            .map(|index| index.creation_time.clone())
     }
 }
