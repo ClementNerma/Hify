21a789042eaf1e3bbadd1e9002551b593ad67169
Clément Nerma <clement.nerma@gmail.com>
2022-04-02 12:50:50 +0200

    Add fullscreen styling

--- hify-client/src/App.svelte
+++ hify-client/src/App.svelte
@@ -5,16 +5,30 @@
   import Home from './pages/Home/Home.svelte'
 </script>
 
-<Router>
-  <Route path="/">
-    <Home />
-  </Route>
+<div class="container">
+  <Router>
+    <Route path="/">
+      <Home />
+    </Route>
 
-  <Route path="/albums">
-    <AlbumGrid />
-  </Route>
+    <Route path="/albums">
+      <AlbumGrid />
+    </Route>
 
-  <Route path="/album/:id" let:params>
-    <AlbumTracks albumId={params['id']} />
-  </Route>
-</Router>
+    <Route path="/album/:id" let:params>
+      <AlbumTracks albumId={params['id']} />
+    </Route>
+  </Router>
+</div>
+
+<style>
+  .container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: lightgray;
+    overflow: auto;
+  }
+</style>
--- hify-client/src/pages/Home/Home.svelte
+++ hify-client/src/pages/Home/Home.svelte
@@ -3,5 +3,4 @@
 </script>
 
 <h1>Hello world!</h1>
-
 <AlbumGrid />
