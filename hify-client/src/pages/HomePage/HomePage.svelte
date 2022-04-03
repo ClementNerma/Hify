a075f143d61463ab1c42adaab8881cb2ef42a40b
Clément Nerma <clement.nerma@gmail.com>
2022-04-03 23:00:33 +0200

    Remake the home page

--- hify-client/src/pages/HomePage/HomePage.svelte
+++ hify-client/src/pages/HomePage/HomePage.svelte
@@ -1,2 +1,19 @@
 <script lang="ts">
+  import { AsyncHomePage } from './HomePage.generated'
+
+  import AlbumsPage from '../AlbumsPage/AlbumsPage.svelte'
+
+  const indexInfos = AsyncHomePage({ variables: {} }).then((res) => res.data.indexInfos)
 </script>
+
+{#await indexInfos}
+  <h1>Loading...</h1>
+{:then indexInfos}
+  <h1>Index fingerprint: <strong><em>{indexInfos.fingerprint}</em></strong></h1>
+
+  <h2>Albums ({indexInfos.albumsCount})</h2>
+
+  <AlbumsPage />
+{:catch e}
+  <h1>Failed to load homepage: {e.message}</h1>
+{/await}
