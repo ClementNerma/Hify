95cd6e15276257d257897815716c254a1a33f158
Clément Nerma <clement.nerma@gmail.com>
2022-04-03 15:33:11 +0200

    Remove empty genres from tracks

--- hify-server/src/index/ffprobe.rs
+++ hify-server/src/index/ffprobe.rs
@@ -153,7 +153,13 @@ fn parse_ffprobe_tags(mut tags: FFProbeTags) -> Result<TrackTags, String> {
 
         genres: tags
             .remove("genre")
-            .map(|genres| genres.split(';').map(str::to_string).collect())
+            .map(|genres| {
+                genres
+                    .split(';')
+                    .filter(|genre| !genre.is_empty())
+                    .map(str::to_string)
+                    .collect()
+            })
             .unwrap_or_default(),
     })
 }
