1e7b7c3d572a3165328e4daf9409af681cf8acb0
Clément Nerma <clement.nerma@gmail.com>
2022-04-03 16:57:14 +0200

    Add lazy-loading support for Grid

--- hify-client/src/organisms/Grid/Grid.svelte
+++ hify-client/src/organisms/Grid/Grid.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import NavigableGrid from '../../navigable/NavigableGrid/NavigableGrid.svelte'
-  import Card from '../../molecules/Card/Card.svelte'
 
   export let columns: number
+  export let lazy: NavigableGrid['$$prop_def']['lazy'] = undefined
 
   const columnsSize = new Array(columns).fill(`${100 / columns}%`).join(' ')
 </script>
 
 <div class="container" style="--columns-size: {columnsSize}">
-  <NavigableGrid {columns}>
+  <NavigableGrid {columns} {lazy}>
     <slot />
   </NavigableGrid>
 </div>
