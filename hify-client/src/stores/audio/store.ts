eb62e4cb9586947a07839342cbe6557f1e5b43f9
Clément Nerma <clement.nerma@gmail.com>
2022-04-03 22:25:48 +0200

    Improve audio progress handling in dedicated module

--- hify-client/src/stores/audio/store.ts
+++ hify-client/src/stores/audio/store.ts
@@ -1,4 +1,5 @@
-import { derived, writable } from 'svelte/store'
+import { derived, get, writable } from 'svelte/store'
+import { AlbumYearStrategy } from '../../graphql/types'
 import { getStreamUri } from '../../rest-api'
 import { AsyncAudioTrack, AudioTrackQuery } from './AudioTrack.generated'
 
@@ -9,8 +10,16 @@ type AudioPlayingState = {
 }
 
 const audioPlaying = writable<AudioPlayingState | null>(null)
+const audioProgress = writable<number | null>(null)
+
+audioPlaying.subscribe((playing) => {
+  if (!playing) {
+    audioProgress.set(null)
+  }
+})
 
 export const readableAudioPlaying = derived(audioPlaying, (_) => _)
+export const readableAudioProgress = derived(audioProgress, (_) => _)
 
 export function playTrack(trackId: string) {
   audioPlaying.update((prevAudio): AudioPlayingState => {
@@ -19,14 +28,39 @@ export function playTrack(trackId: string) {
     const newAudio = new Audio(getStreamUri(trackId))
     newAudio.play().catch((e) => alert('Failed to play audio: ' + (e instanceof Error ? e.message : '<unknown error>')))
 
+    audioProgress.set(0)
+
+    let lastTimeUpdate = 0
+
+    newAudio.addEventListener('timeupdate', () => {
+      const currentTime = Math.round(newAudio.currentTime)
+
+      if (currentTime !== lastTimeUpdate) {
+        lastTimeUpdate = currentTime
+        audioProgress.set(currentTime)
+      }
+    })
+
     return {
       htmlEl: newAudio,
       trackId,
       trackInfos: AsyncAudioTrack({
         variables: {
           trackId,
+          albumYearStrategy: AlbumYearStrategy.IdenticalOrFirstTrack,
         },
       }).then((res) => res.data.track),
     }
   })
+}
+
+export function setPlayingAudioProgress(seconds: number) {
+  const playing = get(audioPlaying)
+
+  if (!playing) {
+    console.warn('Tried to set audio progress while no audio is playing')
+    return
+  }
+
+  playing.htmlEl.currentTime = seconds
 }
