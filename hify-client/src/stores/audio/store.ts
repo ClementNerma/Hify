e653b350e237c62e2919cdbc36c829093a9095cd
Clément Nerma <clement.nerma@gmail.com>
2022-04-03 22:37:18 +0200

    Expose more informations and utilities from audio player's store

--- hify-client/src/stores/audio/store.ts
+++ hify-client/src/stores/audio/store.ts
@@ -11,6 +11,7 @@ type AudioPlayingState = {
 
 const audioPlaying = writable<AudioPlayingState | null>(null)
 const audioProgress = writable<number | null>(null)
+const audioPaused = writable<boolean | null>(null)
 
 audioPlaying.subscribe((playing) => {
   if (!playing) {
@@ -20,15 +21,20 @@ audioPlaying.subscribe((playing) => {
 
 export const readableAudioPlaying = derived(audioPlaying, (_) => _)
 export const readableAudioProgress = derived(audioProgress, (_) => _)
+export const readableAudioPaused = derived(audioPaused, (_) => _)
 
 export function playTrack(trackId: string) {
   audioPlaying.update((prevAudio): AudioPlayingState => {
     prevAudio?.htmlEl.pause()
 
     const newAudio = new Audio(getStreamUri(trackId))
-    newAudio.play().catch((e) => alert('Failed to play audio: ' + (e instanceof Error ? e.message : '<unknown error>')))
-
-    audioProgress.set(0)
+    newAudio
+      .play()
+      .then(() => {
+        audioPaused.set(false)
+        audioProgress.set(0)
+      })
+      .catch((e) => alert('Failed to play audio: ' + (e instanceof Error ? e.message : '<unknown error>')))
 
     let lastTimeUpdate = 0
 
@@ -63,4 +69,35 @@ export function setPlayingAudioProgress(seconds: number) {
   }
 
   playing.htmlEl.currentTime = seconds
+}
+
+export function toggleAudioPlayback() {
+  const playing = get(audioPlaying)
+
+  if (!playing) {
+    console.warn('Tried to toggle audio playback while no audio is playing')
+    return
+  }
+
+  if (playing.htmlEl.paused) {
+    playing.htmlEl
+      .play()
+      .catch((e) => alert('Failed to resume audio: ' + (e instanceof Error ? e.message : '<unknown error>')))
+    audioPaused.set(false)
+  } else {
+    playing.htmlEl.pause()
+    audioPaused.set(true)
+  }
+}
+
+export function humanReadableAudioProgress(seconds: number): string {
+  const hours = Math.floor(seconds / 3600)
+  seconds -= hours * 3600
+
+  const minutes = Math.floor(seconds / 60)
+  seconds -= minutes * 60
+
+  const hoursPrefix = hours > 0 ? `${hours < 10 ? '0' : ''}${hours}:` : ''
+
+  return `${hoursPrefix}${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`
 }
