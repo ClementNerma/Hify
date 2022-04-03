ce9433a2c9c8fe879a1c0c2105204a319dfcccde
Clément Nerma <clement.nerma@gmail.com>
2022-04-03 12:20:29 +0200

    Add a NavigableOne component

---
+++ hify-client/src/navigable/NavigableOne/NavigableOne.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { onDestroy } from 'svelte'
+
+  import { getParentNavigable, setChildrenNavigable } from '../navigation'
+  import { NavigableOne } from './NavigableOne'
+
+  const nav = getParentNavigable()
+  const row = new NavigableOne(nav)
+
+  nav.append(row)
+
+  setChildrenNavigable(row)
+
+  onDestroy(() => nav.remove(row))
+</script>
+
+<slot />
---
+++ hify-client/src/navigable/NavigableOne/NavigableOne.ts
@@ -0,0 +1,41 @@
+import { Navigable, NavigableContainer, NavigableItem, NavigationComingFrom, NavigationDirection } from '../navigation'
+
+export class NavigableOne extends NavigableContainer {
+  private onlyChild: Navigable | null = null
+
+  constructor(parent: NavigableContainer) {
+    super(parent)
+  }
+
+  append(navigable: Navigable): void {
+    if (this.onlyChild) {
+      throw new Error('Navigable ones can only contain a single navigable')
+    }
+
+    this.onlyChild = navigable
+  }
+
+  hasChild(child: Navigable): boolean {
+    return child === this.onlyChild
+  }
+
+  remove(child: Navigable): void {
+    if (!this.onlyChild) {
+      throw new Error('Cannot remove component from empty navigable one')
+    }
+
+    if (this.onlyChild !== child) {
+      throw new Error("Tried to remove another navigable than the navigable's only one")
+    }
+
+    this.onlyChild = null
+  }
+
+  navigate(_: NavigableContainer, direction: NavigationDirection): NavigableItem | null {
+    return this.parent.navigate(this, direction)
+  }
+
+  firstItemDown(from: NavigationComingFrom): NavigableItem | null {
+    return this.onlyChild ? this.onlyChild.firstItemDown(from) : null
+  }
+}
