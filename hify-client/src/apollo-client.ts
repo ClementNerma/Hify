d241a6a968daa87eebe7fd5ae038e36603282af0
Clément Nerma <clement.nerma@gmail.com>
2022-04-05 17:01:49 +0200

    Setup the frontend and API to be exposed to global network

--- hify-client/src/apollo-client.ts
+++ hify-client/src/apollo-client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client/core'
 
-export const API_SERVER_URL = 'http://localhost:8000'
+export const API_SERVER_URL = `http://${location.hostname}:8893`
 
 export default new ApolloClient({
   uri: `${API_SERVER_URL}/graphql`,
--- hify-client/vite.config.js
+++ hify-client/vite.config.js
@@ -1,6 +1,16 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  plugins: [svelte()]
-})
+export default ({mode}) => {
+  const dotenv = require('dotenv')
+  dotenv.config({ path: `./.env.${mode}` })
+
+  return defineConfig({
+    plugins: [svelte()],
+
+    server: {
+      port: process.env['VITE_PORT'] ?? 8892,
+      host: !!process.env['VITE_HOST']
+    }
+  })
+}
