e5dc7eb60c7ef20b04b53db1db667548e5e955cf
Clément Nerma <clement.nerma@gmail.com>
2022-04-05 20:41:53 +0200

    Limit logs output to 100 entries

--- hify-client/src/pages/DevToolsPage/DevToolsPage.svelte
+++ hify-client/src/pages/DevToolsPage/DevToolsPage.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import SimpleNavigableItem from '../../navigable/SimpleNavigableItem/SimpleNavigableItem.svelte'
   import { appLogs } from '../../stores/audio/debugger'
+
+  $: slicedAppLogs = $appLogs.slice(0, 100)
 </script>
 
 <h2>Developer Tools</h2>
@@ -10,7 +12,7 @@
 </SimpleNavigableItem>
 
 <ul>
-  {#each $appLogs as logEntry}
+  {#each slicedAppLogs as logEntry}
     <li class="log-entry {logEntry.level}">
       <u>{logEntry.level.toLocaleUpperCase()}</u>
       <strong>{logEntry.at.toLocaleTimeString()}</strong>: {logEntry.message}
