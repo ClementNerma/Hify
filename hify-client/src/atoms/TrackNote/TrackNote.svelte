4546685c4c80fff31fc5171c6ad10d38f22c5f1f
Clément Nerma <clement.nerma@gmail.com>
2022-09-26 18:35:16 +0200

    Fix: comparison (minor)

--- hify-client/src/atoms/TrackNote/TrackNote.svelte
+++ hify-client/src/atoms/TrackNote/TrackNote.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   export let note: number
 
-  const noteStr = [11, 31, 51, 71, 91].map((min) => (note > min ? '&starf;' : '&star;')).join('')
+  const noteStr = [11, 31, 51, 71, 91].map((min) => (note >= min ? '&starf;' : '&star;')).join('')
 </script>
 
 <span>{@html noteStr}</span>
