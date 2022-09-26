80199258147177a5683e64a8c4c112be73f00cd7
Clément Nerma <clement.nerma@gmail.com>
2022-09-26 18:32:56 +0200

    Add a Checkbox component

---
+++ hify-client/src/atoms/Checkbox/Checkbox.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Button from '../Button/Button.svelte'
+
+  export let checked = false
+  export let onChange: ((checked: boolean) => void) | null = null
+
+  function onPress() {
+    checked = !checked
+    onChange?.(checked)
+  }
+</script>
+
+<Button {onPress}>
+  <input type="checkbox" {checked} />
+  <slot />
+</Button>
