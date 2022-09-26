3af176770971395b8d0f4fdfe2e658de4b60be84
Clément Nerma <clement.nerma@gmail.com>
2022-09-26 17:08:29 +0200

    Add home page with feed generation :tada:

--- hify-client/src/pages/HomePage/HomePage.svelte
+++ hify-client/src/pages/HomePage/HomePage.svelte
@@ -1,17 +1,38 @@
 <script lang="ts">
   import { AsyncHomePage } from '../../graphql/generated'
+  import AlbumsRow from '../../molecules/AlbumsRow/AlbumsRow.svelte'
+  import ArtistsRow from '../../molecules/ArtistsRow/ArtistsRow.svelte'
+  import TracksRow from '../../molecules/TracksRow/TracksRow.svelte'
 
-  import AlbumsPage from '../AlbumsPage/AlbumsPage.svelte'
-
-  const indexInfos = AsyncHomePage({ variables: {} }).then((res) => res.data.indexInfos)
+  const indexInfos = AsyncHomePage({ variables: {} }).then((res) => res.data.generateFeed)
 </script>
 
 {#await indexInfos}
   <h1>Loading...</h1>
-{:then indexInfos}
-  <h2>Albums ({indexInfos.albumsCount})</h2>
+{:then { lastListenedTo, popularTracks, popularAlbums, popularArtists, randomGreatAlbums, randomGreatArtists }}
+  <h2>Last songs you listened to:</h2>
 
-  <AlbumsPage />
+  <TracksRow tracks={lastListenedTo} />
+
+  <h2>Tracks you like to listen to:</h2>
+
+  <TracksRow tracks={popularTracks} />
+
+  <h2>Albums you like to listen to:</h2>
+
+  <AlbumsRow albums={popularAlbums} />
+
+  <h2>Artists you like to listen to:</h2>
+
+  <ArtistsRow artists={popularArtists} />
+
+  <h2>Random great albums:</h2>
+
+  <AlbumsRow albums={randomGreatAlbums} />
+
+  <h2>Random great artists:</h2>
+
+  <ArtistsRow artists={randomGreatArtists} />
 {:catch e}
   <h1>Failed to load homepage: {e.message}</h1>
 {/await}
