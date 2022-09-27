d91f111b9114a1c962250b93d8833b654bfbab87
Clément Nerma <clement.nerma@gmail.com>
2022-09-27 10:37:26 +0200

    Improve command-line options

--- hify-server/src/cmd.rs
+++ hify-server/src/cmd.rs
@@ -5,21 +5,21 @@ use clap::Parser;
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 pub struct Command {
-    #[clap()]
+    #[clap(help = "Path to the directory containing the audio files")]
     pub music_dir: PathBuf,
 
-    #[clap(short, long)]
+    #[clap(short, long, help = "Path to the index file (JSON)")]
     pub index_file: PathBuf,
 
-    #[clap(short, long)]
+    #[clap(short, long, help = "Path to the user data file (JSON)")]
     pub user_data_file: PathBuf,
 
-    #[clap(short, long)]
+    #[clap(long, help = "Force to rebuild index from scratch")]
     pub rebuild_index: bool,
 
-    #[clap(short, long)]
+    #[clap(long, help = "Update the index")]
     pub update_index: bool,
 
-    #[clap(short, long)]
+    #[clap(long, help = "Don't start the server")]
     pub no_server: bool,
 }
