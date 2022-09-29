1aab4ed3d3e647a9e0892b00a1809db5708d9e70
Clément Nerma <clement.nerma@gmail.com>
2022-09-29 21:05:18 +0200

    Update history page with pagination

--- hify-client/src/pages/HistoryPage/HistoryPage.svelte
+++ hify-client/src/pages/HistoryPage/HistoryPage.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { navigate } from 'svelte-navigator'
-
-  import { AsyncHistory } from '../../graphql/generated'
+  import { AsyncHistoryPage, AudioTrackFragment } from '../../graphql/generated'
   import InteractiveCard from '../../molecules/Card/InteractiveCard.svelte'
   import { showContextMenu } from '../../molecules/ContextMenu/ContextMenu.svelte'
   import Grid from '../../organisms/Grid/Grid.svelte'
@@ -11,27 +10,47 @@
   import { bind } from '../../utils'
   import { contextMenuStore } from '../Template/TplContextMenu.svelte'
 
-  const history = AsyncHistory({
-    variables: {},
-    fetchPolicy: 'no-cache',
-  })
+  const TRACKS_PER_LINE = 7
+  const LINES_PER_PAGE = 5
+
+  let currentPageInfo: { endCursor?: string | null; hasNextPage: boolean } | null = null
+
+  const feedMore = async () => {
+    if (currentPageInfo?.hasNextPage === false) {
+      return
+    }
+
+    const res = await AsyncHistoryPage({
+      variables: {
+        pagination: {
+          after: currentPageInfo?.endCursor,
+          first: TRACKS_PER_LINE * LINES_PER_PAGE,
+        },
+      },
+    })
+
+    currentPageInfo = res.data.history.pageInfo
+    tracks = [...tracks, ...res.data.history.nodes]
+  }
+
+  let tracks: AudioTrackFragment[] = []
 </script>
 
-{#await history}
+{#await feedMore()}
   <h2>Loading...</h2>
-{:then history}
-  <h2>History: {history.data.history.length} tracks</h2>
+{:then _}
+  <h2>History</h2>
 
   <Grid columns={7}>
-    {#each history.data.history as track, i}
+    {#each tracks as track, i}
       {@const tags = track.metadata.tags}
 
       <InteractiveCard
         title={tags.title}
         subtitle={tags.album.name}
         pictureUrl={getAlbumArtUri(tags.album.id)}
-        onPress={bind({ history, i }, ({ history, i }) => {
-          playTrackFromNewQueue(history.data.history, i)
+        onPress={bind(i, (i) => {
+          playTrackFromNewQueue(tracks, i)
           navigate(ROUTES.nowPlaying)
         })}
         onLongPress={bind(tags.album, (album) =>
