6ea4af3d8d2eb39a2ce262697797c2b6e19d078f
Clément Nerma <clement.nerma@gmail.com>
2022-09-29 16:28:45 +0200

    Add a 'Column' atom

---
+++ hify-client/src/atoms/Column/Column.svelte
@@ -0,0 +1,17 @@
+<script>
+  import NavigableList from '../../navigable/NavigableList/NavigableList.svelte'
+</script>
+
+<div class="column">
+  <NavigableList>
+    <slot />
+  </NavigableList>
+</div>
+
+<style>
+  .column {
+    display: flex;
+    flex-direction: column;
+    overflow-x: auto;
+  }
+</style>
