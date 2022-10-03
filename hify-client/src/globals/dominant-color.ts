a2872c21223c00602e2146991a533fcceb8517c6
Clément Nerma <clement.nerma@gmail.com>
2022-10-03 21:07:04 +0200

    Use a new color extraction algorithm

--- hify-client/src/globals/dominant-color.ts
+++ hify-client/src/globals/dominant-color.ts
@@ -1,5 +1,6 @@
 import { Color } from '../stores/custom-bg-color'
-import { ColorThief } from './color-thief.lib'
+import { logDebug } from '../stores/debugger'
+import kmeans from './kmeans'
 
 export async function computeDominantColor(imgUrl: string): Promise<Color> {
   const img = new Image()
@@ -12,9 +13,12 @@ export async function computeDominantColor(imgUrl: string): Promise<Color> {
     throw new Error('Undefined image width and/or height')
   }
 
+  const width = Math.min(img.width, 100)
+  const height = (img.height / img.width) * width
+
   const canvas = document.createElement('canvas')
-  canvas.width = img.width
-  canvas.height = img.height
+  canvas.width = width
+  canvas.height = height
 
   const ctx = canvas.getContext('2d')
 
@@ -24,15 +28,24 @@ export async function computeDominantColor(imgUrl: string): Promise<Color> {
 
   ctx.drawImage(img, 0, 0)
 
-  const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height).data
+  const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height).data
   const pixelsCount = canvas.width * canvas.height
 
-  if (pixels.length !== pixelsCount * 4) {
+  if (imgData.length !== pixelsCount * 4) {
     throw new Error('Invalid image data length from temporary canvas')
   }
 
-  const c = new ColorThief()
-  const [r, g, b] = c.getColor(img)
+  const pixels = new Array(pixelsCount)
+    .fill(null)
+    .map((_, i) => [imgData[i * 4], imgData[i * 4 + 1], imgData[i * 4 + 2]])
 
-  return [r, g, b, 0.5]
+  const started = Date.now()
+
+  const { centroids } = kmeans(pixels, 5)
+
+  logDebug(`Computed centroids K-Means in ${Date.now() - started} ms.`)
+
+  const [r, g, b] = centroids[0]
+
+  return [Math.round(r), Math.round(g), Math.round(b), 0.5]
 }
