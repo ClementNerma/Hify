d543190636bfb002e586873ac60d1cb65544482b
Clément Nerma <clement.nerma@gmail.com>
2022-10-06 13:57:23 +0200

    Update background brightness balance

--- hify-client/src/pages/NowPlayingPage/NowPlayingPageBackground.svelte
+++ hify-client/src/pages/NowPlayingPage/NowPlayingPageBackground.svelte
@@ -20,7 +20,7 @@
     const color = track?.metadata.tags.album.art?.dominantColor ?? { r: 0, g: 0, b: 0 }
 
     const centerColor = changeBrightness(color, 0.9)
-    const extColor = changeBrightness(color, 0.6)
+    const extColor = changeBrightness(color, 0.4)
 
     return `radial-gradient(circle, ${colorToRGB(centerColor)} 0%, ${colorToRGB(extColor)} 100%)`
   }
