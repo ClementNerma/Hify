3d6b8cb427c59ed7753788a6d54bc6f4e0870578
Clément Nerma <clement.nerma@gmail.com>
2022-10-25 10:39:26 +0200

    Rename ID scalars as String to improve codegen

--- hify-server/src/graphql/mod.rs
+++ hify-server/src/graphql/mod.rs
@@ -14,7 +14,7 @@ pub use state::{GraphQLContext, SaveIndexFn};
 #[macro_export]
 macro_rules! define_scalar_string {
     ($typename: ident) => {
-        #[::async_graphql::Scalar]
+        #[::async_graphql::Scalar(name = "String")]
         impl ::async_graphql::ScalarType for $typename {
             fn parse(value: ::async_graphql::Value) -> ::async_graphql::InputValueResult<Self> {
                 if let ::async_graphql::Value::String(value) = value {
