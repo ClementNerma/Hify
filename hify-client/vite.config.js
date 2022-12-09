320c0c48d56d861ea4bd5eeda2a4b0e3e3114376
Clément Nerma <clement.nerma@gmail.com>
2022-12-09 21:22:16 +0100

    Update client dependencies

--- hify-client/vite.config.js
+++ hify-client/vite.config.js
@@ -2,9 +2,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { defineConfig } from 'vite'
 import wasm from 'vite-plugin-wasm'
 import topLevelAwait from 'vite-plugin-top-level-await'
+import dotenv from 'dotenv'
 
-export default ({ mode }) => {
-  const dotenv = require('dotenv')
+export default ({ mode }) => {  
   dotenv.config({ path: `./.env.${mode}` })
 
   return defineConfig({
