2429bbbe222f531d8c027c225d3684b5c6c5fc1b
Clément Nerma <clement.nerma@gmail.com>
2023-03-19 23:19:39 +0100

    Remove obsolete comment

--- hify-client/src/atoms/ImgLoader/ImgLoader.svelte
+++ hify-client/src/atoms/ImgLoader/ImgLoader.svelte
@@ -1,15 +1,8 @@
 <script lang="ts">
-  /**
-   * Note: As both BlurHash decoding and Canvas painting + encoding is slow (both taking about 2 ms each)
-   *       this component's features were removed, until a more performant way is found
-   *
-   *       The component's properties are left for compatibility reasons and future-proofing.
-   */
+  import { getArtUri } from "../../globals/rest-api";
+  import { ProgressiveImgFragment } from "../../graphql/generated";
 
-  import { getArtUri } from '../../globals/rest-api'
-  import { ProgressiveImgFragment } from '../../graphql/generated'
-
-  export let art: ProgressiveImgFragment | null | undefined
+  export let art: ProgressiveImgFragment | null | undefined;
 </script>
 
 {#if art}
