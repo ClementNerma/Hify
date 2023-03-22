d44013176532a2a4876fb4afe0dff2367aaa647e
Clément Nerma <clement.nerma@gmail.com>
2023-03-22 15:20:51 +0100

    Make the stats box focusable

--- hify-client/src/molecules/StatsBox/StatsBox.svelte
+++ hify-client/src/molecules/StatsBox/StatsBox.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { AsyncGenerateStats } from "../../graphql/generated";
+  import SimpleNavigableItem from "../../navigable/headless/SimpleNavigableItem/SimpleNavigableItem.svelte";
   import { humanReadableDuration } from "../../stores/audio-player";
 
   const statsReq = AsyncGenerateStats({
@@ -10,34 +11,36 @@
   $: stats = statsReq.then((stats) => stats.data.generateStats);
 </script>
 
-{#await stats}
-  <strong>Loading...</strong>
-{:then stats}
-  <table>
-    <tr>
-      <td>Total number of tracks</td>
-      <td><strong>{stats.tracksCount}</strong></td>
-    </tr>
-    <tr>
-      <td>Total number of albums</td>
-      <td><strong>{stats.albumsCount}</strong></td>
-    </tr>
-    <tr>
-      <td>Total number of album artists</td>
-      <td><strong>{stats.albumArtistsCount}</strong></td>
-    </tr>
-    <tr>
-      <td>Total number of artists</td>
-      <td><strong>{stats.artistsCount}</strong></td>
-    </tr>
-    <tr>
-      <td>Number of listened tracks</td>
-      <td><strong>{stats.totalTracksListened}</strong></td>
-    </tr>
-    <tr>
-      <td>Total listening duration</td>
-      <td><strong>{humanReadableDuration(stats.totalListeningTime)}</strong></td
-      >
-    </tr>
-  </table>
-{/await}
+<SimpleNavigableItem>
+  {#await stats}
+    <strong>Loading...</strong>
+  {:then stats}
+    <table>
+      <tr>
+        <td>Total number of tracks</td>
+        <td><strong>{stats.tracksCount}</strong></td>
+      </tr>
+      <tr>
+        <td>Total number of albums</td>
+        <td><strong>{stats.albumsCount}</strong></td>
+      </tr>
+      <tr>
+        <td>Total number of album artists</td>
+        <td><strong>{stats.albumArtistsCount}</strong></td>
+      </tr>
+      <tr>
+        <td>Total number of artists</td>
+        <td><strong>{stats.artistsCount}</strong></td>
+      </tr>
+      <tr>
+        <td>Number of listened tracks</td>
+        <td><strong>{stats.totalTracksListened}</strong></td>
+      </tr>
+      <tr>
+        <td>Total listening duration</td>
+        <td><strong>{humanReadableDuration(stats.totalListeningTime)}</strong></td
+        >
+      </tr>
+    </table>
+  {/await}
+</SimpleNavigableItem>
