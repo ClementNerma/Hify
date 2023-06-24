95f29567c2eef797150918bf40b3aa91d32e9d98
Clément Nerma <clement.nerma@gmail.com>
2023-06-24 13:28:39 +0200

    Fix: some problems with rating management

--- hify-client/src/atoms/ModifiableTrackRating/ModifiableTrackRating.svelte
+++ hify-client/src/atoms/ModifiableTrackRating/ModifiableTrackRating.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
     import { AudioTrackFragment, SetTrackRating } from "../../graphql/generated";
     import SimpleNavigableItem from "../../navigable/headless/SimpleNavigableItem/SimpleNavigableItem.svelte";
+    import { bind } from "../../globals/utils";
 
     export let track: AudioTrackFragment
+    
+    let initialRating: number | null
 
-    let initialRating = track.appOnlyRating ?? track.metadata.tags.rating
+    let current: number | null
+    let updating: boolean
+    let failed: boolean
 
-    let current = initialRating
-    let updating = false
-    let failed = false
+    $: {
+        initialRating = track.appOnlyRating ?? track.metadata.tags.rating
+        current = initialRating
+        updating = false
+        failed = false
+    }
 
     async function update() {
         const updatingWith = current
@@ -22,22 +30,26 @@
         failed = !!done.errors
         current = updatingWith
         initialRating = updatingWith
+
+        // Not ideal but required because re-fetching the whole tracks list
+        // would be both complex and inefficient
+        track.appOnlyRating = updatingWith
     }
 
-    function inc(relative: number) {
-        current += relative
+    function setRating(newRating: number) {
+        current = newRating
         failed = false
     }
 </script>
 
 <SimpleNavigableItem
-    onLeft={current >= 2 ? () => inc(-2) : undefined}
-    onRight={current <= 8 ? () => inc(+2) : undefined}
+    onLeft={current === null ? undefined : current >= 2 ? bind(current, (current) => setRating(current - 2)) : undefined}
+    onRight={current === null ? () => setRating(2) : current <= 8 ? bind(current, (current) => setRating(current + 2)) : undefined}
     onPress={() => { update() }}
 >
     <div class:changed={current !== initialRating} class:updating class:failed={failed}>
         {#each [2, 4, 6, 8, 10] as value}
-            {#if current >= value}
+            {#if current !== null && current >= value}
                 &starf;
             {:else}
                 &star;
