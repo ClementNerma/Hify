457a1cf13d442bcdf9f9039063c0db143fbd8ba8
Clément Nerma <clement.nerma@gmail.com>
2023-08-18 17:31:57 +0200

    Add comment for Android TV integration API

--- hify-client/src/globals/injected.ts
+++ hify-client/src/globals/injected.ts
@@ -1,5 +1,12 @@
+/**
+ * @file Module used for integration with the Android TV application
+ */
+
 declare const hifyAndroidInjectedObject: {
 	updateAppUrl(): void
 }
 
+/**
+ * Integration API for the Android TV application
+ */
 export const hifyInterface = 'hifyAndroidInjectedObject' in window ? hifyAndroidInjectedObject : null
