36e9a09faf99e44dcce1f3cdf5ca3cd6cb22f3dc
Clément Nerma <clement.nerma@gmail.com>
2023-08-30 23:24:37 +0200

    Improve HTTP error handling

--- hify-server/src/http/logging.rs
+++ hify-server/src/http/logging.rs
@@ -7,7 +7,7 @@ pub async fn log_errors<B>(request: Request<B>, next: Next<B>) -> Response {
 
     let res = next.run(request).await;
 
-    if !res.status().is_success() && !res.status().is_redirection() {
+    if res.status().is_client_error() || res.status().is_server_error() {
         error!(
             "{} {}",
             res.status().as_u16().to_string().bright_red(),
