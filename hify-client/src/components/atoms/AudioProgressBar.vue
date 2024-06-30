e950047edc8d6712934a903e22f74d8d117fced3
Clément Nerma <clement.nerma@gmail.com>
2024-06-30 23:51:50 +0200

    Change audio progress bar's accent color when focused

--- hify-client/src/components/atoms/AudioProgressBar.vue
+++ hify-client/src/components/atoms/AudioProgressBar.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { NavigationDirection } from '@/navigable';
 import NavigableItem from '@/navigable/vue/components/NavigableItem.vue';
+import { computed, ref } from 'vue';
 
 defineProps<{
     max: number,
@@ -8,13 +9,16 @@ defineProps<{
     onDirection: (direction: 'left' | 'right') => void,
     onPress?: () => void
 }>()
+
+const focused = ref(false)
 </script>
 
 <template>
     <NavigableItem :intercept-key-press="dir => dir === NavigationDirection.Left || dir === NavigationDirection.Right"
-        @left-key="onDirection('left')" @right-key="onDirection('right')" :on-press v-slot="{ focused }">
-        <div class="border border-solid border-transparent" :class="{ 'border-gray-400': focused }">
-            <input type="range" ref="inputRef" class="h-2.5" :max :value />
+        @focus="focused = true" @unfocus="focused = false" @left-key="onDirection('left')"
+        @right-key="onDirection('right')" :on-press v-slot="{ focused }">
+        <div class="border border-solid border-transparent">
+            <input type="range" ref="inputRef" class="h-2.5" :max :value :class="{ 'accent-orange-400': focused }" />
         </div>
     </NavigableItem>
 </template>
