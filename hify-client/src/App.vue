bcb09d9af5a8a0360b912d10e70c376a57589841
Clément Nerma <clement.nerma@gmail.com>
2024-07-01 19:17:07 +0200

    Move page layout file

--- hify-client/src/App.vue
+++ hify-client/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import Layout from './views/Layout.vue'
+import Layout from './Layout.vue'
 import { provideClient } from '@urql/vue';
 import { gqlClient } from './global/urql-client';
 
