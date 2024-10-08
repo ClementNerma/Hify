e12dfb9fd56abc27bbfd3510cee839e0d52649ac
Clément Nerma <clement.nerma@gmail.com>
2024-10-08 20:58:03 +0200

    Fix: context menu appearing in wrong place

--- hify-client/src/components/organisms/ContextMenu.vue
+++ hify-client/src/components/organisms/ContextMenu.vue
@@ -11,7 +11,7 @@ onUpdated(() => {
     return
   }
 
-  const container = containerRef.value
+  const ctxMenuContainer = containerRef.value
   const column = listRef.value ?? logFatal('Column reference not initialized yet')
 
   const focusedItemId = getFocusedItemId()
@@ -26,8 +26,8 @@ onUpdated(() => {
   const top = rect ? (rect.top + rect.bottom) / 2 : 0
   const left = rect ? (rect.left + rect.right) / 2 : 0
 
-  ctxTop.value = top + container.clientHeight > window.innerHeight ? window.innerHeight - container.clientHeight - 5 : top
-  ctxLeft.value = left + container.clientWidth > window.innerWidth ? window.innerWidth - container.clientWidth - 5 : left
+  ctxTop.value = top + ctxMenuContainer.clientHeight > window.innerHeight ? window.innerHeight - ctxMenuContainer.clientHeight - 5 : top
+  ctxLeft.value = left + ctxMenuContainer.clientWidth > window.innerWidth ? window.innerWidth - ctxMenuContainer.clientWidth - 5 : left
 
   prevFocusItemId.value = focusedItemId
 
@@ -56,7 +56,7 @@ const containerRef = ref<HTMLDivElement | null>(null)
   <NavigableList v-if="contextMenuStore && contextMenuStore.options.length > 0"
     :intercept-key-press="dir => dir === NavigationDirection.Back" @back-key="closeContextMenu">
     <div
-      class="fixed bg-gray-800 text-white border border-solid border-gray-600 z-10 shadow-[2px_2px_5px_rgb(60,60,60)]"
+      class="ctxmenu fixed bg-gray-800 text-white border border-solid border-gray-600 z-10 shadow-[2px_2px_5px_rgb(60,60,60)]"
       ref="containerRef" :style="`top: ${ctxTop}px; left: ${ctxLeft}px;`">
       <!-- TODO: implement "trapped" -->
       <NavigableList trapped ref="listRef">
