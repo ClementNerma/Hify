00960b99fbbb6bd1351a56dbea74cf163f8d1f69
Clément Nerma <clement.nerma@gmail.com>
2025-03-21 15:30:15 +0100

    Load more items at once when continuing mix

--- hify-client/src/global/constants.ts
+++ hify-client/src/global/constants.ts
@@ -5,7 +5,7 @@ export const API_SERVER_URL = `http://${location.hostname}:8893`
 export const LARGE_MIX_TRACKS_QTY = 100
 export const DISPLAYABLE_ITEMS_QTY = 25
 export const MIN_GREAT_RATING = 8
-export const EXTEND_MIX_TRACKS_QTY = 20
+export const EXTEND_MIX_TRACKS_QTY = 100
 
 export const GRID_TRACKS_PER_ROW = 7
 export const GRID_TRACKS_PRELOAD_ROWS = 5
