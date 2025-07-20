6708bf4420815c30d35b4c8db2484b3ed184556c
Clément Nerma <clement.nerma@mail.com>
2025-07-20 23:15:35 +0200

    Fix: TypeScript error

--- hify-client/src/components/molecules/ArtistCard.vue
+++ hify-client/src/components/molecules/ArtistCard.vue
@@ -4,13 +4,13 @@ import { getArtistArtUrl, MIN_GREAT_RATING } from '@/global/constants'
 import { type ContextMenuOption, showContextMenu } from '@/global/stores/context-menu'
 import { generateAndPlayMix } from '@/global/stores/play-queue'
 import { type ArtistFragment, MixOrdering } from '@/graphql/generated/graphql'
-import NavigableItem from '@/navigable/vue/components/NavigableItem.vue'
+import NavigableItem, { type NavigableItemProps } from '@/navigable/vue/components/NavigableItem.vue'
 import router from '@/router'
 import Card from './Card.vue'
 
-const { artist } = defineProps<{
+const { artist, ...navigableItemProps } = defineProps<{
 	artist: ArtistFragment
-}>()
+} & NavigableItemProps>()
 
 const contextMenuOptions = computed<ContextMenuOption[]>(() => [
 	{
@@ -28,8 +28,8 @@ const contextMenuOptions = computed<ContextMenuOption[]>(() => [
 </script>
 
 <template>
-    <NavigableItem @press="router.push({ name: 'artist', params: { id: artist.id } })"
-        @long-press="showContextMenu(contextMenuOptions)">
-        <Card :title="artist.name" :art-url="getArtistArtUrl(artist)" circle />
-    </NavigableItem>
+	<NavigableItem @press="router.push({ name: 'artist', params: { id: artist.id } })"
+		@long-press="showContextMenu(contextMenuOptions)" v-bind="navigableItemProps">
+		<Card :title="artist.name" :art-url="getArtistArtUrl(artist)" circle />
+	</NavigableItem>
 </template>
