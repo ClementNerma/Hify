8e36c40baa2d334c985116b619dbebbf055db753
Clément Nerma <clement.nerma@gmail.com>
2026-07-11 08:15:57 +0200

    feat(server): log faulty requests' query

--- hify-server/src/server/utils/logging.rs
+++ hify-server/src/server/utils/logging.rs
@@ -4,16 +4,18 @@ use log::{debug, error};
 
 pub async fn log_errors(request: Request, next: Next) -> Response {
     let path = request.uri().path().to_owned();
+    let query = request.uri().query().map(str::to_owned);
     let method = request.method().as_str().to_owned();
 
     let res = next.run(request).await;
 
     if res.status().is_client_error() || res.status().is_server_error() {
         error!(
-            "{} {} {}",
+            "{} {} {} {}",
             res.status().as_u16().to_string().bright_red(),
             method.bright_blue().underline(),
-            path.bright_yellow()
+            path.bright_yellow(),
+            query.unwrap_or_default().bright_black()
         );
     } else {
         debug!(
