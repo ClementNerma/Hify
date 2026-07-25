1ef2e49a3d3bc5cbba0807e51f1c64310f5e5364
Clément Nerma <clement.nerma@gmail.com>
2026-07-25 21:59:13 +0200

    feat(ui): completely remove item titles from context menus

--- hify-client/src/global/ctx-menu.tsx
+++ hify-client/src/global/ctx-menu.tsx
@@ -34,7 +34,7 @@ export const defaultCtxMenus = {
       },
       {
         icon: <FaCompactDisc />,
-        label: `Go to album: ${track.album.album.name}`,
+        label: 'Go to album',
         onPress: () => {
           navigate(routes.album, { albumId: track.album.album.id })
         },
@@ -43,7 +43,7 @@ export const defaultCtxMenus = {
       ...track.artists.map(
         ({ artist }): ContextMenuItem => ({
           icon: <FaMicrophoneLines />,
-          label: `Go to artist: ${artist.name}`,
+          label: 'Go to artist',
           onPress: () => {
             navigate(routes.artist, { artistId: artist.id })
           },
@@ -93,7 +93,7 @@ export const defaultCtxMenus = {
     ...artists.map(
       ({ artist }): ContextMenuItem => ({
         icon: <FaMicrophoneLines />,
-        label: `Go to artist: ${artist.name}`,
+        label: 'Go to artist',
         onPress: () => {
           navigate(routes.artist, { artistId: artist.id })
         },
